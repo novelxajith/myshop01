c94f14a96a37c5fbef4e69fc9836b4b8d0c2783a
Ajith <117803558+novelxajith@users.noreply.github.com>
2024-03-30 21:11:03 +0530

    frondend & registration/login/add product commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $pageName['pageName'] = 'index';
+        return view('ecommerce/index',$pageName); 
+    }
+
+    public function signin()
+    {
+        return view('ecommerce/sign-in'); 
+    }
+
+    public function signup()
+    {
+        return view('ecommerce/sign-up'); 
+    }
+    public function product_details()
+    {
+        $pageName['pageName'] = 'categories';
+        return view('ecommerce/product_details',$pageName); 
+    }
+    public function products()
+    {
+        $pageName['pageName'] = 'products';
+        return view('ecommerce/products',$pageName); 
+    }
+    public function checkout()
+    {
+        $pageName['pageName'] = 'checkout';
+        return view('ecommerce/checkout',$pageName); 
+    }
+    public function cart()
+    {
+        $pageName['pageName'] = 'cart';
+        return view('ecommerce/cart',$pageName); 
+    }
+    public function product()
+    {
+        $pageName['pageName'] = 'product';
+        return view('product',$pageName); 
+    }
+    public function checkoute()
+    {
+        $pageName['pageName'] = 'checkout';
+        return view('checkout',$pageName); 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController; // Fix the uppercase "C"
+use App\Http\Controllers\LoginRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,68 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Ecommerce route
+Route::middleware(['user'])->group(function () {
+
+Route::get('/product_details', [\App\Http\Controllers\Controller::class, 'product_details'])->name('product_details');
+Route::get('/products', [\App\Http\Controllers\Controller::class, 'products'])->name('products');
+Route::get('/checkout', [\App\Http\Controllers\Controller::class, 'checkout'])->name('checkout');
+Route::get('/cart', [\App\Http\Controllers\Controller::class, 'cart'])->name('cart');
+Route::get('/main', [\App\Http\Controllers\Controller::class, 'main'])->name('main');
+Route::get('/product', [\App\Http\Controllers\Controller::class, 'product'])->name('product');
+Route::middleware(['user.logout:u'])->get('/logout/user', 'Auth\LoginRegisterController@logout_user')->name('user.logout');
 });
+Route::get('/', [\App\Http\Controllers\Controller::class, 'index'])->name('index');
+Route::get('/signin', [\App\Http\Controllers\Controller::class, 'signin'])->name('signin');
+Route::get('/signup', [\App\Http\Controllers\Controller::class, 'signup'])->name('signup');
+Route::post('/ecommerce_login', [\App\Http\Controllers\LoginRegisterController::class, 'ecommerce_login'])->name('ecommerce_login');
+
+
+// admin route
+Route::middleware(['admin','auth:admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [\App\Http\Controllers\AdminController::class, 'index'])->name('indexx');
+    Route::get('/add_product', [\App\Http\Controllers\AdminController::class, 'add_product'])->name('add_product');
+    Route::get('/product_list', [\App\Http\Controllers\AdminController::class, 'product_list'])->name('product_list');
+    Route::get('/orders', [\App\Http\Controllers\AdminController::class, 'orders'])->name('orders');
+    Route::get('/order_details', [\App\Http\Controllers\AdminController::class, 'order_details'])->name('order_details');
+    Route::get('/add_category', [\App\Http\Controllers\AdminController::class, 'add_category'])->name('add_category');
+
+    
+    //Route::middleware(['admin.logout:a'])->get('/logout/admin', 'Auth\LoginRegisterController@logout_admin')->name('admin.logout');
+   Route::post('/logout', [\App\Http\Controllers\LoginRegisterController::class, 'admin_logout'])->name('admin_logout');
+
+   Route::post('/main_post', [\App\Http\Controllers\AdminProductController::class, 'main_category_post'])->name('main_category_post');
+   Route::post('/sub_post', [\App\Http\Controllers\AdminProductController::class, 'sub_category_post'])->name('sub_category_post');
+   Route::post('/create_product', [\App\Http\Controllers\AdminProductController::class, 'create_product_post'])->name('create_product_post');
+   Route::post('/gallery_upload', [\App\Http\Controllers\AdminProductController::class, 'gallery_upload_post'])->name('gallery_upload_post');
+
+});
+Route::get('/admin', [\App\Http\Controllers\AdminController::class, 'signin'])->name('adminSignin');
+Route::post('/adminLogin', [\App\Http\Controllers\LoginRegisterController::class, 'admin_login'])->name('admin_login');
+
+
+
+
+
+
+
+
+
+
+
+
+//user route
+Route::get('vendor/', [\App\Http\Controllers\VendorController::class, 'index'])->name('vendorindex');
+Route::get('vendor/dashboard', [\App\Http\Controllers\VendorController::class, 'dashboard'])->name('vendordashboard');
+
+//login post
+//Route::get('login_post', [\App\Http\Controllers\LoginRegisterController::class, 'login'])->name('login_post');
+Route::post('/login_', [\App\Http\Controllers\LoginRegisterController::class, 'login'])->name('login_post');
+
+Route::post('/logout', [\App\Http\Controllers\LoginRegisterController::class, 'logout'])->name('logout');
